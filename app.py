7b657a8d1c46af2a4d50a98542202e3159f9878d
Vedit Firat Arig <firat@vngrs.com>
2017-02-15 20:14:13 +0300

    Fixed embarerssing bug

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "%s %s" % ('Hello DevopsMeetup')
+    return "%s %s" % ('Hello', 'DevopsMeetup')
 
 
 if __name__ == '__main__':
